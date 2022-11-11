f4285ec76937e523ece92fa4da7269bf0fd87d8d
Mavsic <muminova.m@hotmail.com>
2022-11-10 19:44:36 -0500

    css refactoring

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Auth from "./utils/auth";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+// import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
+
+import Admin from './pages/admin';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  const token = Auth.loggedIn() ? Auth.getProfile() : null
+  console.log(token)
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/admin" 
+                element={<Admin />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              {/* <Route 
+                path="/success" 
+                element={<Success />} 
+              /> */}
+              <Route 
+                path="/orderHistory" 
+                element={<OrderHistory />} 
+              />
+              {/* <Route 
+                path="/products/:id" 
+                element={<Detail />} 
+              /> */}
+
+            </Routes>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
