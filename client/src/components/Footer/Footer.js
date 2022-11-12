9f062f2149b0bd3259b1b8a15feec64c73938660
Mavsic <muminova.m@hotmail.com>
2022-11-11 21:17:10 -0500

    styling

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+  return (
+    <footer > 
+    <div className='glow'>
+        Made with love and collaboration
+      </div>  
+    </footer>
+  )
+}
+
+export default Footer
